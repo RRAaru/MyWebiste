d857553aa579759208218c120d646ae17f9a08fb
RRAaru <100890852+RRAaru@users.noreply.github.com>
2023-05-07 01:06:46 +0800

    Ver.0.5

--- script.js
+++ script.js
@@ -3,9 +3,10 @@ const buttonBox=document.querySelector('#buttonBox');
 const ghost=document.querySelectorAll('#ghost');
 const follow = document.getElementById('follow');
 const odiv=document.querySelector('.main');
-let number=0
+const body=document.getElementById('body');
+let number=0;
 img.onclick=function(){
-    img.classList='imgClick'
+    img.classList='imgClick';
     buttonBox.classList='buttonBox';
 }
 
@@ -16,8 +17,10 @@ document.addEventListener('mousemove', function(e) {
     div_y=odiv.getBoundingClientRect().top;
     if(x>=div_x & x<=div_x+800 & y>=div_y & y<=div_y+400){
         follow.classList='none';
+        body.style.cursor='auto';
     }
     else{
+        body.style.cursor='none';
         follow.classList='follow';
         follow.style.left = x - 50 + 'px';
         follow.style.top = y - 50 + 'px';
@@ -27,16 +30,16 @@ document.addEventListener('mousemove', function(e) {
 ghost.forEach(element => {
     div_x=odiv.getBoundingClientRect().left;
     div_y=odiv.getBoundingClientRect().top;
-    number+=1
-    element.classList='ghost'+number
+    number+=1;
+    element.classList='ghost'+number;
     const set=document.querySelector('.ghost'+number);
     element.addEventListener('click', function(){
         set.src='img/dead.png';
         set.style.opacity=1;
     });
     while (true) {
-        x=Math.floor(Math.random() * (1800 - 0 + 1)) + 0 
-        y=Math.floor(Math.random() * (850 - 0 + 1)) + 0
+        x=Math.floor(Math.random() * (1800 - 0 + 1)) + 0 ;
+        y=Math.floor(Math.random() * (850 - 0 + 1)) + 0 ;
         set.style.left=x+'px';
         set.style.top=y+'px';
         if(x>=div_x-50 & x<=div_x+850 & y>=div_y-50 & y<=div_y+450){
